dce87f3ab7bef8120d4a1b38c8c8a8457abef2d3
msj <2669159659@qq.com>
2021-11-30 20:07:25 +0800

    yeb登录页面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <el-form ref="form" :model="loginForm">
-      <h3>系统登录</h3>
+    <el-form ref="form" :model="loginForm" class="loginContainer">
+      <h3 class="loginTitle">系统登录</h3>
       <el-form-item>
         <el-input type="text" auto-complete="false" v-model="loginForm.username" placeholder="请输入用户名"></el-input>
       </el-form-item>
@@ -9,10 +9,10 @@
         <el-input type="password" auto-complete="false" v-model="loginForm.password" placeholder="请输入密码"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-input type="text" auto-complete="false" v-model="loginForm.code" placeholder="点击图片更换验证码"></el-input>
-        <img src="" alt="">
+        <el-input type="text" auto-complete="false" v-model="loginForm.code" placeholder="点击图片更换验证码" style="width: 250px;margin-right: 5px"></el-input>
+        <img :src="captchaUrl" alt="">
       </el-form-item>
-      <el-checkbox v-model="checked">记住我</el-checkbox>
+      <el-checkbox v-model="checked" class="loginRemember">记住我</el-checkbox>
       <el-button type="primary" style="width:100%">登录</el-button>
     </el-form>
   </div>
@@ -27,12 +27,33 @@ export default {
         username: 'admin',
         password: '123',
         code: ''
-      }
+      },
+      captchaUrl: '',
+      checked: true
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
+.loginContainer {
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 180px auto;
+  width: 350px;
+  padding: 15px 35px 15px 35px;
+  background: #ffff;
+  border: 1px solid #c6c4c4;
+  box-shadow: 0 0 25px #cac6c6;
+}
 
+.loginTitle {
+  margin: 8px auto 48px auto;
+  text-align: center;
+}
+
+.loginRemember {
+  text-align: left;
+  margin: 0px 0px 15px 0px;
+}
 </style>
