2441d2b75129b18a5f123d3b8eb14a2e996bc7f6
msj <2669159659@qq.com>
2021-11-30 19:53:13 +0800

    登录页雏形

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-    Login
+    <el-form ref="form" :model="loginForm">
+      <h3>系统登录</h3>
+      <el-form-item>
+        <el-input type="text" auto-complete="false" v-model="loginForm.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="password" auto-complete="false" v-model="loginForm.password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="text" auto-complete="false" v-model="loginForm.code" placeholder="点击图片更换验证码"></el-input>
+        <img src="" alt="">
+      </el-form-item>
+      <el-checkbox v-model="checked">记住我</el-checkbox>
+      <el-button type="primary" style="width:100%">登录</el-button>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
